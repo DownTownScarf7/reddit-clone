baacbd268030e38c7256649804f2776954ea5249
DownTownScarf7 <vbence741@gmail.com>
2018-07-06 15:05:55 +0200

    added 404 page

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const http = new XMLHttpRequest();
+let user = window.location.pathname.substr(1);
 
 document.querySelector('#post-btn').onclick = () => {
   const titleElem = document.querySelector('#post-title'),
     urlElem = document.querySelector('#post-content'),
     title = titleElem.value,
     url = urlElem.value,
-    owner = 'Test User';
+    owner = user;
   if (title.replace(/\s/g,'') && url.replace(/\s/g,'') && owner) {
     http.open('POST', `http://localhost:3000/api/posts`, true);
     http.setRequestHeader("Content-Type", "application/json");
@@ -23,48 +24,57 @@ document.querySelector('#post-btn').onclick = () => {
   urlElem.value = title.replace(/\s/g,'') ? null : url;
 }
 
-http.open('GET', 'http://localhost:3000/api/posts', true);
-http.setRequestHeader("Content-Type", "application/json");
-http.onload = () => {
-  let res = null;
-  try {
-    res = JSON.parse(http.responseText).posts;
-  }
-  catch (err) {
-    console.error(err);
-    return;
-  }
-  console.log(res);  // DEBUG
-  if (res) {
-    const clickDel = event => {
-      event.target.parentNode.remove();
-      http.open('DELETE', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}`, true);
-      http.send();
+function getPosts() {
+  http.open('GET', 'http://localhost:3000/api/posts', true);
+  http.setRequestHeader("Content-Type", "application/json");
+  http.onload = () => {
+    let res = null;
+    try {
+      res = JSON.parse(http.responseText).posts;
     }
+    catch (err) {
+      console.error(err);
+      return;
+    }
+    
+    console.log(res);  // DEBUG
+    if (res) {
+      const clickDel = event => {
+        event.target.parentNode.remove();
+        http.open('DELETE', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}`, true);
+        http.send();
+      }
 
-    const upDoot = event => {
-      http.open('PUT', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}/upvote`, true);
-      http.send();
-    }
+      const upDoot = event => {
+        http.open('PUT', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}/upvote`, true);
+        http.send();
+      }
 
-    const downDoot = event => {
-      http.open('PUT', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}/downvote`, true);
-      http.send();
+      const downDoot = event => {
+        http.open('PUT', `http://localhost:3000/api/posts/${event.target.parentNode.dataset.id}/downvote`, true);
+        http.send();
+      }
+      const postElemets = res.map(elem => (
+        <div id={'post_' + elem.id} data-id={elem.id} className="post" key={elem.id}>
+          <p className='post-title'>{elem.title}</p>
+          <p className='post-owner'>{elem.owner}</p>
+          <p className='post-timestamp'>{elem.timestamp}</p>
+          <h3 className='post-score'>{elem.score}</h3>
+          <p className='post-url'>{elem.url}</p>
+          <button className='post-btn up' onClick={upDoot}>UP</button>
+          <button className='post-btn down' onClick={downDoot}>DOWN</button>
+          <button className='post-btn edit'>edit</button>
+          <button className='post-btn delete' onClick={clickDel}>delete</button>
+        </div>
+      ));
+      ReactDOM.render(postElemets, document.querySelector('#posts'));
     }
-    const postElemets = res.map(elem => (
-      <div id={'post_' + elem.id} data-id={elem.id} className="post" key={elem.id}>
-        <p className='post-title'>{elem.title}</p>
-        <p className='post-owner'>{elem.owner}</p>
-        <p className='post-timestamp'>{elem.timestamp}</p>
-        <h3 className='post-score'>{elem.score}</h3>
-        <p className='post-url'>{elem.url}</p>
-        <button className='post-btn up' onClick={upDoot}>UP</button>
-        <button className='post-btn down' onClick={downDoot}>DOWN</button>
-        <button className='post-btn edit'>edit</button>
-        <button className='post-btn delete' onClick={clickDel}>delete</button>
-      </div>
-    ));
-    ReactDOM.render(postElemets, document.querySelector('#posts'));
   }
+  http.send();
 }
-http.send();
+
+if (user) {
+  getPosts();
+} else {
+  window.location = 'http://localhost:3000/';
+}
